a242dcea1cc026b598dc04471f1998869e76650e
Karuna Issa <karunaissa400@gmail.com>
2025-05-16 12:20:21 +0530

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,9 +1,5 @@
-#BEFORE FIXING THE BUG ---
-#def withdraw(amount, balance):
-    #return balance - amount  # ❌ Bug: No check for negative balance!
+def withdraw(amount, balance):
+    if amount > balance:
+        raise ValueError("Insufficient funds!")
+    return balance - amount
 
-#After fixing the bug ---
- def withdraw(amount, balance):
-     if amount > balance:
-         raise ValueError("Insufficient funds!")  # ✅ Prevents negative balance
-     return balance - amount
